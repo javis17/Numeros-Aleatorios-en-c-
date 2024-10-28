36d7323d0e96c66d1157fa4b19e89ae14c61b55c
Javis <javieralvarez172005@gmail.com>
2024-10-28 18:27:55 -0500

    Variables intento y numeroIntentos Intento: Almacena cada intento del usuario. numeroIntentos: Lleva el conteo del número de intentos realizados.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@
         {
             Random random = new Random();
             int numeroPredefinido = random.Next(1,10);
+            int intento;
+            int numeroIntentos = 0;
         }
     }
 }
