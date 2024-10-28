2248a260dcc46c42a10336767be97afd75db0399
Javis <javieralvarez172005@gmail.com>
2024-10-28 18:18:05 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Numeros_Aleatorios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
